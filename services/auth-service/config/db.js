160ad779d6d027762873e4a6ab0af0f900217c22
jayanishehara2003 <jayanishehara29@gmail.com>
2026-03-09 11:48:49 +0530

    update new vacancy

--- services/auth-service/config/db.js
+++ services/auth-service/config/db.js
@@ -1,8 +1,13 @@
 const mongoose = require('mongoose');
+const dns = require('dns');
+
+dns.setServers(['8.8.8.8', '8.8.4.4']);
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI, {
+      family: 4
+    });
     console.log('MongoDB Connected ✅');
   } catch (err) {
     console.error('MongoDB connection error:', err);
