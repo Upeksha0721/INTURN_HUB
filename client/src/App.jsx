d77b63e275ea1d691a5c8efb7c86e7ee572d75a7
GeethyaRuwandi <it23740174@my.sliit.lk>
2026-03-28 10:56:18 +0530

    Work in progress: update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,7 +15,6 @@ import PostVacancy from './pages/admin/PostVacancy';
 import ManageVacancies from './pages/admin/ManageVacancies';
 import ManageApplications from './pages/admin/ManageApplications';
 import UploadMaterial from './pages/admin/UploadMaterial';
-import CreateQuiz from './pages/admin/CreateQuiz';
 import LandingPage from './pages/LandingPage';
 import Messages from './pages/admin/Messages';
 import CVBuilder from './pages/student/CVBuilder';
@@ -95,9 +94,7 @@ function AppRoutes() {
       <Route path="/admin/upload-material" element={
         <ProtectedRoute adminOnly={true}><AdminLayout><UploadMaterial /></AdminLayout></ProtectedRoute>
       } />
-      <Route path="/admin/create-quiz" element={
-        <ProtectedRoute adminOnly={true}><AdminLayout><CreateQuiz /></AdminLayout></ProtectedRoute>
-      } />
+      
       <Route path="/admin/messages" element={
         <ProtectedRoute adminOnly={true}><AdminLayout><Messages /></AdminLayout></ProtectedRoute>
       } />
