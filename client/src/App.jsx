e61f5a224393c0ce8d5ad7a9eeed45b079c5951e
GeethyaRuwandi <it23740174@my.sliit.lk>
2026-04-05 16:42:19 +0530

    update my flies

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -108,7 +108,7 @@ function AppRoutes() {
        <ProtectedRoute><StudentLayout><Quizzes /></StudentLayout></ProtectedRoute>
       } />
 
-      <Route path="/admin/create-quiz" element={
+      <Route path="/admin/quizzes" element={
        <ProtectedRoute><AdminLayout><ManageQuizzes /></AdminLayout></ProtectedRoute>
       } />
     </Routes>
